55594dc58fe8a20877ba035d973657e78c4aafba
ChannaS <channasandakelum7@gmail.com>
2024-06-27 15:33:37 +0530

    create student.java and main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+       
+        Student student = new Student("Kamal");
+
+       
+        String studentName = student.getName();
+        System.out.println("Student Name: " + studentName);
+    }
+}
